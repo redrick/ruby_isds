82a54fb795f1127038e578d6a81aa2e12726b620
Andrej Antas <andrej@antas.cz>
2018-06-19 16:39:30 +0200

    Still specs...

--- lib/ruby_isds/responses/body.rb
+++ lib/ruby_isds/responses/body.rb
@@ -4,8 +4,15 @@ module RubyIsds
       def initialize(response)
         (response.keys - self.class::REMOVED_KEYS).each do |key|
           self.class.send(:attr_accessor, key.to_sym)
-          instance_variable_set("@#{key}", response[key])
+          instance_variable_set("@#{key}", parsed_value(response[key]))
         end
+      end
+
+      private
+
+      def parsed_value(value)
+        return value unless value.is_a?(Hash)
+        value.keys.first == 'xsi:nil' && value.values.first == 'true' ? nil : false
       end
     end
   end
--- spec/ruby_isds/data_message_spec.rb
+++ spec/ruby_isds/data_message_spec.rb
@@ -1,2 +1,29 @@
 RSpec.describe RubyIsds::DataMessage do
+  describe '.find' do
+    it 'correctly call underlying web service' do
+      expect(::RubyIsds::WebServices::DmOperations::MessageDownload)
+        .to receive(:call).with(dmID: '123456789')
+
+      RubyIsds::DataMessage.find('123456789')
+    end
+  end
+
+  context 'with real message' do
+    before do
+      VCR.use_cassette 'data_box/received' do
+        @message = RubyIsds::DataBox.received.messages.last
+      end
+    end
+
+    describe '#author' do
+      it 'displays author information' do
+        VCR.use_cassette 'data_message/author' do
+          @author = @message.author
+        end
+
+        expect(@author.body.userType).to eq('OFFICIAL')
+        expect(@author.body.authorName).to be_nil
+      end
+    end
+  end
 end
